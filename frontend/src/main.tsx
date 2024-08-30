9165ff378ce1376355a5de4e261bd482aeb71f08
sa-gems-github <sa-gems-github@dfinity.org>
2024-08-30 17:38:25 -0400

    0191a539-2d36-7eec-b949-8d13fabf1ef3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,10 +7,10 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#3498db',
+      main: '#8e44ad',
     },
     secondary: {
-      main: '#2c3e50',
+      main: '#9b59b6',
     },
     error: {
       main: '#e74c3c',
